08a5d46e8cfb26051feae3bab6cb309c25fdd71d
johan <johanossa697@hotmail.com>
2025-07-29 15:53:20 -0500

    Johan Portfolio

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,33 +5,9 @@ import { autoNewTabExternalLinks } from './src/autoNewTabExternalLinks';
 import robotsTxt from 'astro-robots-txt';
 import partytown from '@astrojs/partytown';
 import tailwindcss from '@tailwindcss/vite';
-import tailwind from '@astrojs/tailwind';
-import staticAdapter from '@astrojs/static';
+import github from '@astrojs/github';
 
 export default defineConfig({
-	site: 'https://johanfuertv.github.io/PortafolioOssa', // 👈 Tu URL de GitHub Pages
-	base: '/PortafolioOssa', // 👈 Carpeta base del repositorio
-	integrations: [
-		mdx(),
-		sitemap(),
-		partytown(),
-		robotsTxt(),
-		tailwind({ applyBaseStyles: false })
-	],
-	markdown: {
-		extendDefaultPlugins: true,
-		rehypePlugins: [
-			[
-				autoNewTabExternalLinks,
-				{
-					domain: 'johanfuertv.github.io'
-				}
-			]
-		]
-	},
-	vite: {
-		plugins: [tailwindcss()]
-	},
-	output: 'static',
-	adapter: staticAdapter()
-});
+  site: 'https://johanfuertv.github.io/PortafolioOssa/',
+  base: 'https://github.com/johanfuertv/PortafolioOssa',
+})
